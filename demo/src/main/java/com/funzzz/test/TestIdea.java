52e5391131c2b89e8d0b3bf7cf0a20a3d34b117e
zhoudongdong <2013881276@qq.com>
2019-10-11 14:57:29 +0800

    first commit

---
+++ demo/src/main/java/com/funzzz/test/TestIdea.java
@@ -0,0 +1,10 @@
+package com.funzzz.test;
+
+import java.util.Date;
+
+public class TestIdea {
+    public static void main(String[] args) {
+        System.out.printf("hahah");
+        new Date();
+    }
+}
